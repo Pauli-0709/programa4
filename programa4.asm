eb91510bb5a9938a714111f12c0b7e891bdc47d0
Pauli-0709 <mariapaulinacamachoherrera@gmail.com>
2026-08-23 15:26:31 -0600

    subiendo programa de captura de teclas

--- programa4.asm
+++ programa4.asm
@@ -1,14 +1,42 @@
 ;inicio del codigo
 section .data
+	mensaje: db 'presione una tecla y luego enter' ,0xa
+	mensaje_tamano: equ $-mensaje
 	db variable ;almacenamiento de la tecla
 
 ;bandera de texto
 section .text
-	global_start
+	global _start
+	global _mensaje
+	global _registro_datos
+	global _tecla
+	global _enter
+	global _guardar_enter
 
 _start:
-mov rax,0
-mov rdi,0
-mov rsi,variable
-mov rdx,N
-syscall
+	;impresion de texto 1
+	mov rax,1
+	mov rdi,1
+	mov rsi,mensaje1
+	mov rdx,mensaje_tamano
+
+	;inicio de la segunda bandera
+_mensaje:
+	syscall ;syscall para primer bloque
+
+	mov rax,0
+	mov rdi,0
+	mov rsi,variable
+	mov rdx,N
+	
+_registro_datos:; bandera para datos
+	syscall ;syscall para segundo bloque
+
+_tecla: ;bandera para tecla
+	;bloque para capturar el enter
+	mov rax,0
+	mov rdi,0
+	mov rsi,enter
+	mov rdx,N
+
+_guardar_enter:
